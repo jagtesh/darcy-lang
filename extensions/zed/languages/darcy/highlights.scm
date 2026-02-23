7941e2bedb89bb8848ed5c987febe42d76130f1a
Jag Chadha <jagtesh@gmail.com>
2026-02-23 10:02:05 -0500

    fix: use valid zed highlights query syntax

--- extensions/zed/languages/darcy/highlights.scm
+++ extensions/zed/languages/darcy/highlights.scm
@@ -1,31 +1,28 @@
 ["(" ")" "[" "]" "{" "}"] @punctuation.bracket
 
 (number) @number
+(character) @constant.builtin
 (boolean) @constant.builtin
-(string) @string
-(comment) @comment
-(block_comment) @comment
-(directive) @comment
-
-; symbols and keywords
 (symbol) @variable
 
-((symbol) @keyword
- (#match? @keyword "^(def|defn|defin|defmacro|defrecord|defenum|extern|export|if|when|cond|case|do|let|let!|fn|loop|while|for|break|continue|require|quote|syntax-quote|unquote|unquote-splicing|with-meta|call|range|range-incl|true|false|nil)$"))
+(string) @string
 
-((symbol) @operator
- (#match? @operator "^(\+|-|\*|/|mod|=|>|<|>=|<=|&|\||->|->>)$"))
+(escape_sequence) @escape
 
-; callable form head
 (list
   .
   (symbol) @function)
 
-; namespaced call heads
+((symbol) @operator
+ (#match? @operator "^(\\+|-|\\*|/|=|>|<|>=|<=|mod|&|\\||->|->>)$"))
+
 (list
   .
-  (symbol) @function.method
-  (#match? @function.method "^[a-z][a-z0-9._-]*/[a-zA-Z0-9_?!*+=<>$-]+$"))
+  (symbol) @keyword
+  (#match? @keyword
+   "^(def|defn|defin|defmacro|defrecord|defenum|extern|export|if|when|cond|case|do|let|let!|fn|loop|while|for|break|continue|require|quote|syntax-quote|unquote|unquote-splicing|with-meta|call|range|range-incl|and|or|true|false|nil)$"
+   ))
 
-; :keyword literals
-((keyword) @property)
+[(comment)
+ (block_comment)
+ (directive)] @comment
